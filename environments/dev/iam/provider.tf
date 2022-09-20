ed8a7f36db7ddaabcc1740bbda7bd30cda1c8ca1
Renz <crc1046@dlsud.edu.ph>
2022-09-20 15:40:38 +0800

    provided credentials for iam

--- environments/dev/iam/provider.tf
+++ environments/dev/iam/provider.tf
@@ -1,5 +1,9 @@
+locals {
+  envs = { for tuple in regexall("(.*)=(.*)", file(".env")) : tuple[0] => tuple[1] }
+}
+
 provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region = ""
+    access_key = local.envs["ACCESS_KEY"]
+    secret_key = local.envs["SECRET_KEY"]
+    region = local.envs["REGION"]
 }
